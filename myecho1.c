baf93509d6729282ca811d62793230f128362334
Preveen P <wide4head@gmail.com>
2022-07-14 19:11:52 +0530

    added prompt for echo program

--- myecho1.c
+++ myecho1.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-char end_command[] = "bye";
+char end_command[] = "bye\n";
+char prompt[] = "> ";
 
 int main()
 {
-	printf("Will echo back what is typed, type %s to end\n", end_command);
+	printf("Will echo back what is typed, to end type %s", end_command);
+	printf("%s", prompt);
 
 	while(1) {
 		char message[512] = "";
-		gets(message);
+		// gets(message); // dangerous
+		fgets(message, sizeof(message), stdin); // safe
 		if (strcmp(message, end_command) == 0) 
 			goto end;
-		puts(message);
+		printf("%s", message);
+		printf("%s", prompt);
 	}
 end:
 	puts("Good Bye, Have nice day. Tune your vibration, be in non resistive, allowing mode\n");
