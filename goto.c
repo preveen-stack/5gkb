1a1e351bfe2bfd5241182af06f8f185684fe1643
Preveen P <wide4head@gmail.com>
2022-07-14 17:39:41 +0530

    improved my echo program

--- goto.c
+++ goto.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 char message[] = "Hello World";
+char filename[] = "goto.c";
 
-int main() {
+int main(int argc, char* argv[]) {
+
+	printf("%s\n", filename); 
 
 	printf("%s\n", message);
 
--- myecho.c
+++ myecho.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+
+char end_command[] = "bye";
 
 int main()
 {
+	printf("Will echo back what is typed, type %s to end\n", end_command);
+
 	while(1) {
 		char message[512] = "";
 		scanf("%s", message);
+		if (strcmp(message, end_command) == 0) 
+			goto end;
 		printf("%s\n", message);
 	}
+end:
+	printf("Good Bye, Have nice day. Tune your vibration, be in non resistive, allowing mode\n");
+	return 0;
 }
